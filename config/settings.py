a8d1d39c55c98f9f6dadba0066690125318e3715
Kazunari MATSUMOTO <s1f102003672@iniad.org>
2022-08-30 01:06:19 +0900

    add model "StampCourse", "Stamp", "StampHistory" and "Inset"

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/files/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'files')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
