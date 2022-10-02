45929a04d90b93aece3b5e36a5592b8c33c2113e
aoba-s <s1f102100614@iniad.org>
2022-10-02 14:13:18 +0900

    Add header and footer design

--- client/components/Layouts/Portal/Footer.vue
+++ client/components/Layouts/Portal/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
-		<p>© INIAD-FES executive committee</p>
+	<div class="bg-gray-900 text-white">
+		<p class="text-xs text-center text-gray-200 p-2">Copyright © 2022 INIAD-FES 実行委員会</p>
 	</div>
 </template>
--- client/components/Layouts/Portal/Header.vue
+++ client/components/Layouts/Portal/Header.vue
@@ -1,7 +1,16 @@
 <template>
 	<header>
-		<nuxt-link to="/">INIAD-FES</nuxt-link>
-		<p v-if="isqr">{{ visitor }}</p>
+		<div class="group rounded-b-3xl shadow-xl bg-white px-4 py-2">
+			<div class="flex justify-between">
+				<div class="flex-none">
+					<nuxt-link to="/"><img class="h-12" src="/fes-logo.png" alt="iniadfes-logo"></nuxt-link>
+				</div>
+				<div class="peer-flex-none flex items-center text-sm truncate ml-4">
+					<p v-if="isqr">{{ visitor }} さん</p>
+					<button v-if="!isqr" class="bg-pink-900 hover:bg-pink-700 rounded-full text-white p-1 px-3">ネームカードと連動させる</button>
+				</div>
+			</div>
+		</div>
 	</header>
 </template>
 
