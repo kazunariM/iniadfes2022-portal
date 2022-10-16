d319a5c50353f8640c2cb8640e5e24cfe95227d2
Kazunari MATSUMOTO <s1f102003672@iniad.org>
2022-10-16 22:43:29 +0900

    説明文の修正

--- server/config/settings.py
+++ server/config/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
