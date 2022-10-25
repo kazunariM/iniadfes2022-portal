c62a6f17462f46826725de33c26c28603f3b1bd8
Kazunari MATSUMOTO <s1f102003672@iniad.org>
2022-10-26 04:41:08 +0900

    事前登録期間であることを設定できるように変更
    管理サイトの設定を変更

--- server/fes2022/admin.py
+++ server/fes2022/admin.py
@@ -39,32 +39,72 @@ class CampusAdmin(admin.ModelAdmin):
 
 class RoomAdmin(admin.ModelAdmin):
     readonly_fields = ("uuid",)
-    list_display = ("campus", "groupname", "count", "unique_count", "total_count")
+    list_display = ("campus", "groupname", "count", "unique_count", "total_count", "only_stamp")
     list_filter = ("only_stamp", "have_a_stamp", "campus",)
     list_display_links = ("groupname",)
     search_fields = ("groupname",)
+    ordering = ("campus", "floor", "room_number")
+
+
+class PlaceIDAdmin(admin.ModelAdmin):
+    readonly_fields = ("uuid",)
+    list_display = ("room_campus", "placeid", "door_number", "placenum")
+    ordering = ("placeid",)
+    
+    def room_campus(self, obj):
+        return obj.room.campus
+    
+    room_campus.admin_order_field = "room__campus"
+
+
+class NowCampusAdmin(admin.ModelAdmin):
+    list_display = ("pk", "visitor", "inorout", "scanned_at")
+    readonly_fields = ("pk", "scanned_at")
+    ordering = ("-pk", )
 
 
 class NowRoomAdmin(admin.ModelAdmin):
     list_display = ("pk", "visitor", "inorout", "room", "scanned_at")
     list_filter = ("room",)
     readonly_fields = ("pk", "scanned_at")
+    ordering = ("-pk", )
+
     
-    
+class AreaAdmin(admin.ModelAdmin):
+    pass
+
+
+class StampCourseAdmin(admin.ModelAdmin):
+    pass
+
+
+class StoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class StampAdmin(admin.ModelAdmin):
+    pass
+
+
+class StampHistoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class InsetAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(models.NamecardDesign, NamecardDesignAdmin)
 admin.site.register(models.NamecardPool, NamecardPool)
 admin.site.register(models.Visitor, VisitorAdmin)
 admin.site.register(models.Campus, CampusAdmin)
 admin.site.register(models.Room, RoomAdmin)
-
-admin.site.register(models.PlaceID)
-admin.site.register(models.NowCampus)
-
+admin.site.register(models.PlaceID, PlaceIDAdmin)
+admin.site.register(models.NowCampus, NowCampusAdmin)
 admin.site.register(models.NowRoom, NowRoomAdmin)
-
-admin.site.register(models.Area)
-admin.site.register(models.StampCourse)
-admin.site.register(models.Story)
-admin.site.register(models.Stamp)
-admin.site.register(models.StampHistory)
-admin.site.register(models.Inset)
+admin.site.register(models.Area, AreaAdmin)
+admin.site.register(models.StampCourse, StampCourseAdmin)
+admin.site.register(models.Story, StoryAdmin)
+admin.site.register(models.Stamp, StampAdmin)
+admin.site.register(models.StampHistory, StampHistoryAdmin)
+admin.site.register(models.Inset, InsetAdmin)
